4b5fb9ef1ec18b3042a5dbd8f90bf50ca9947641
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-25 09:24:51 -0700

    psuedocoding initial steps for script.js

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,14 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 })
+
+// Have current day set everytime the application is opened by using moment.js
+
+//Have each time block color coded to match which part of the day (past, present, future), by using conditional to check current time
+
+//Make sure user can enter content into timeblock
+
+//When user clicks save button - set item to local storage
+
+//When user refreshes page info needs to be saved - get item from local storage
+
