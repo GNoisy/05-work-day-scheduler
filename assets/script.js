419914f55f1768b962e101cee1e456530db30af9
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-25 19:43:51 -0700

    created array of row id's to loop through and give class names for time

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,28 @@ $(document).ready(function () {
     // Have current day set everytime the application is opened by using moment.js
 
     //Have each time block color coded to match which part of the day (past, present, future), by using conditional to check current time
+    var date = new Date();
+    // date.setHours(9,0,0);
+    // console.log(date);
+    var rowNum = ["row-nine", "row-ten", "row-eleven", "row-twelve", "row-one", "row-two", "row-three", "row-four", "row-five"];
 
-    //Make sure user can enter content into timeblock
+    //Sets past, present, future class on each row
+    for (var i=0; i<9; i++) {
+        date.setHours(9 + i,0,0,0);
+        var row = document.getElementById(rowNum[i]);
+        var diff = moment().diff(date, "hours");
+        console.log(diff);
+        if (diff === 0) {
+            $(row).addClass("present");
+        } 
+        else if (diff > 0) {
+            $(row).addClass("past");
+        }
+        else {
+            $(row).addClass("future");
+        }
+    }
+
 
     //When user clicks save button - set item to local storage
 
